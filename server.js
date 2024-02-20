96bfae03831122904dcfd3b615773cda5a00d7bb
your_name_here <rautsagar214@gmail.com>
2024-02-20 15:47:11 +0530

    render-deployment

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;;
 
+app.get('/',(req,res)=>{
+  res.send("hello world")
+})
 
 app.get('/ping',(req,res)=>{
   res.send("pong")
