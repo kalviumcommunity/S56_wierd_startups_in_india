a1cdd00b570bc291c7cb950722f76d5abe36a184
your_name_here <rautsagar214@gmail.com>
2024-04-03 11:20:42 +0530

    form working properly

--- Frontend/src/Component/Form.jsx
+++ Frontend/src/Component/Form.jsx
@@ -1,9 +1,11 @@
 import React, { useState,useEffect } from 'react'
-import { Link } from 'react-router-dom';
+import {  useNavigate } from 'react-router-dom';
 import "./Form.css"
 import axios from "axios"
+
+
 const Form = () => {
- 
+    const navigate = useNavigate()
     const [factory,setfactory]= useState("")
     const [inventor,setinventor]= useState("")
     const [material,setmaterial]= useState("")
@@ -13,35 +15,36 @@ const Form = () => {
     const [createdBy, setCreatedBy] = useState("")
 
 
-    useEffect(() => {
-      const cookieValue = document.cookie.split(";")[0].split("=")[1];
-      console.log(cookieValue)
-      setCreatedBy(cookieValue);
-  }, [])
 
-
-    console.log(factory , inventor , material , summary , cost , condition)
-    const Submit = (e)=>{
-        e.preventDefault();
-        axios.post("https://s56-wierd-startups-in-india.onrender.com/poststartup",{
-            company:factory,
-            founder:[inventor],
-            product:material,
-            description:summary,
-            valuation:cost,
-            status:condition,
-            createdby: createdBy
-        
-        })
-        .then(result=>console.log(result.data))
-        .catch(err=>console.log(err))
+  const Submit = (e) => {
+    e.preventDefault();
     
+    const formData = {
+      company: factory,
+      founder: [inventor],
+      product: material,
+      description: summary,
+      valuation: cost,
+      status: condition,
+      createdby: createdBy
+    };
+  console.log(formData,"sagar data ")
 
-       
-        
-        
-    }
+    axios.post("http://localhost:3200/poststartup", formData)
+      .then(result => console.log(result))
+      navigate("/")
+      .catch(err => console.log(err));
+
+
     
+  }
+  
+    
+  useEffect(() => {
+    const cookieValue = document.cookie.split(";")[0].split("=")[1];
+    console.log(cookieValue)
+    setCreatedBy(cookieValue);
+}, [])
 
   return (
     <div>
@@ -52,7 +55,7 @@ const Form = () => {
         <input type="text" name="description" placeholder='Description' required onChange={(e)=>setsummary(e.target.value)}  />
         <input type="text" name="valuation" placeholder='valuation' required  onChange={(e)=>setcost(e.target.value)}  />
         <input type="text" name="status" placeholder='status' required onChange={(e)=>setcondition(e.target.value)}  />
-        <Link  to={"/"}> <button>Submit</button></Link>
+         <button>Submit</button>
       </form>
     </div>
   )
